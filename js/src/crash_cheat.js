a3484d5a095604b24ab01b235664798901e84de6
lssen <lshuangsen@gmail.com>
2019-01-17 20:34:51 +0800

    Site updated: 2019-01-17 20:34:50

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = '�q(��A��`)�r Love u ~';
+         document.title = '= _ = Love u ~';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '(?>��<*?) U come back ~' + OriginTitle;
+         document.title = '(?>w<*?) U come back ~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
